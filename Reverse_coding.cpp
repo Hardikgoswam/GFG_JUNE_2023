ac8db2564b53fb4b050ad88b9168b00ac6a6c96c
Hardik Goswami <hardikgoswami795@gmail.com>
2023-06-21 17:20:53 +0530

    today  21/06/2023

---
+++ Reverse_coding.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int sumOfNaturals(int n) {
+        
+        // sum of N natural number's formula is : n(n+1)/2
+        int  x = 1e9+7;
+        long long y = ((long long)(n%x)*((n%x)+1))/2;
+        return y%x;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        
+        cin>>n;
+
+        Solution ob;
+        cout << ob.sumOfNaturals(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
